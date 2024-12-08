974b9d770fc4ced6cc6de18cca63c85d2534a605
imran <zunaeede@gmail.com>
2024-12-08 11:38:06 +0600

    main - added more content on home page.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,10 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Home Page</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet"> <!-- Optional: Link CSS -->
 </head>
 <body>
-    <h1>Hello laravel</h1>
+    <header>
+        <h1>Welcome to My Laravel App!</h1>
+    </header>
+    <main>
+        <p>This is a basic home page.</p>
+    </main>
+    <footer>
+        <p>&copy; {{ date('Y') }} My Laravel App</p>
+    </footer>
 </body>
 </html>
