459901010976f50c24f9c198def1409f013fac94
imran <zunaeede@gmail.com>
2024-12-11 18:04:12 +0600

    main - concat the code.

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -7,7 +7,10 @@
   {{-- Bootstrap link --}}
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
-<body class="bg-light"> <div class="container mt-5"> <div class="row justify-content-center"> <div class="col-md-6 col-lg-4"> 
+<body class="bg-light">
+   <div class="container mt-5"> 
+    <div class="row justify-content-center"> 
+      <div class="col-md-6 col-lg-4"> 
   <h1 class="text-center mb-4">Admin Login Page</h1>
         <form action="">
             <div class="form-group mb-3">
