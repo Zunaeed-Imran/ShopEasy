2ee8b2273ff1fd9a36c51790c5ba6a4eee15cabc
imran <zunaeede@gmail.com>
2024-12-11 17:39:18 +0600

    main - add route for auth admin.

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,4 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/', [AdminAdminController::class, 'login'])->name('admin.login');
+Route::get('admin/auth', [AdminAdminController::class, 'auth'])->name('admin.auth');
