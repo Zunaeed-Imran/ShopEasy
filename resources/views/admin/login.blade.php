c8feca2c91e61185a5750c3ebfd7ef8ac3c563e9
imran <zunaeede@gmail.com>
2024-12-17 11:01:38 +0600

    main - added all the blade file - added a style page in public folder - set route in web file.

---
+++ resources/views/admin/login.blade.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Admin Login Page</title>
+  {{-- Bootstrap link --}}
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body class="bg-light">
+   <div class="container mt-5"> 
+    <div class="row justify-content-center"> 
+      <div class="col-md-6 col-lg-4"> 
+        @session('error')
+            <div class="alert alert-danger">
+              {{session('error')}}
+            </div>
+        @endsession
+  <h1 class="text-center mb-4">Admin Login Page</h1>
+        <form action="{{route('admin.auth')}}" method="post">
+          @csrf
+            <div class="form-group mb-3">
+              <label for="email" class="form-label">Email</label>
+              <input type="email" class="form-control @error('email')
+                is-invalid
+              @enderror" id="email" name="email" placeholder="Enter email">
+              @error('email')
+                <span class="invalid-feedback">
+                  <strong>{{$message}}</strong>
+                </span>
+              @enderror
+            </div>
+            <div class="form-group mb-3">
+              <label for="password" class="form-label @error('password')
+                is-invalid
+              @enderror">Password</label>
+              <input type="password" class="form-control" id="password" name="password" placeholder="Enter password">
+              @error('password')
+                <span class="invalid-feedback">
+                  <strong>{{$message}}</strong>
+                </span>
+              @enderror
+            </div>
+            <button type="submit" class="btn btn-primary d-block mx-auto">Login</button> 
+        </form>
+      </div>
+    </div>
+  </div>
+  {{-- Bootstrap cdn --}}
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [AdminController::class, 'login'])->name('admin.login');
+Route::post('admin/auth', [AdminController::class, 'auth'])->name('admin.auth');
+
+// Route::get('admin/dashboard', [AdminAdminController::class, 'index'])->name('admin.index');
+
+Route::middleware('admin')->group(function () {
+    Route::get('admin/dashboard', [AdminController::class, 'index'])->name('admin.index');
 });
