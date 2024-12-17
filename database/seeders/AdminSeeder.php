e5ca93143032693ee0c43354cf479726f3c3a23c
imran <zunaeede@gmail.com>
2024-12-17 16:31:10 +0600

    main - added content in admin factory - added content on admin seeder - added database seeder.

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Admin;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class AdminSeeder extends Seeder
 {
@@ -13,5 +14,6 @@ class AdminSeeder extends Seeder
     public function run(): void
     {
         //
+        Admin::factory(1)->create();
     }
 }
