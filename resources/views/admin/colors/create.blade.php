fecff704376f6cf29c48d59de56c9558c775767c
imran <zunaeede@gmail.com>
2024-12-19 17:03:12 +0600

    main - making frontend for colors.

---
+++ resources/views/admin/colors/create.blade.php
@@ -0,0 +1,21 @@
+@extends('admin.layouts.app')
+
+@section('title')
+  Add new color
+@endsection
+
+@section('content')
+  <div class="row">
+    @include('admin.layouts.sidebar')
+    <div class="col-md-9">
+        <div class="row mt-2">
+            <div class="col-md-12">
+                <div class="card-header bg-white">
+                    <h1 class="mt-2">Colors Dashboard</h1>
+                    <hr>
+                </div>
+            </div>
+        </div>
+    </div>
+  </div>
+@endsection
---
+++ resources/views/admin/colors/index.blade.php
@@ -0,0 +1,13 @@
+<div class="row">
+    @include('admin.layouts.sidebar')
+    <div class="col-md-9">
+        <div class="row mt-2">
+            <div class="col-md-12">
+                <div class="card-header bg-white">
+                    <h1 class="mt-2">Colors Dashboard</h1>
+                    <hr>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
