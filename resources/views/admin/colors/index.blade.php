647f40c9e14375b318d53d55351e271002058bc0
imran <zunaeede@gmail.com>
2024-12-21 18:36:45 +0600

    main - working on the display.

--- resources/views/admin/colors/index.blade.php
+++ resources/views/admin/colors/index.blade.php
@@ -1,13 +1,42 @@
+@extends('admin.layouts.app')
+
+@section('title')
+Add new color
+@endsection
+
+@section('content')
 <div class="row">
-    @include('admin.layouts.sidebar')
-    <div class="col-md-9">
-        <div class="row mt-2">
-            <div class="col-md-12">
-                <div class="card-header bg-white">
-                    <h1 class="mt-2">Colors Dashboard</h1>
-                    <hr>
-                </div>
-            </div>
+  @include('admin.layouts.sidebar')
+  <div class="col-md-9">
+    <div class="row mt-2">
+      <div class="col-md-12">
+        <div class="card-header bg-white">
+          <h1 class="mt-2">Colors ({{$colors->count()}})</h1>
+          <hr>
         </div>
+        {{-- table of colors --}}
+        <div class="card-body">
+          <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">First</th>
+                <th scope="col">Last</th>
+                <th scope="col">Handle</th>
+              </tr>
+            </thead>
+            <tbody>
+              @foreach ($colors as $color)
+              <tr>
+                <th scope="row">{{$color->id}}</th>
+                <td>{{$color->name}}</td>
+              </tr>
+              @endforeach
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
+  </div>
 </div>
+@endsection
