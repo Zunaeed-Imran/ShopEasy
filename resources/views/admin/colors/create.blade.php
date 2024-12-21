8ea42821a32357689a401a558149b4ce83c80cac
imran <zunaeede@gmail.com>
2024-12-21 12:42:58 +0600

    main - set store in the form.

--- resources/views/admin/colors/create.blade.php
+++ resources/views/admin/colors/create.blade.php
@@ -11,25 +11,29 @@ Add new color
     <div class="row mt-2">
       <div class="col-md-12">
         <div class="card-header bg-white">
-          <h1 class="mt-2">Colors Dashboard</h1>
+          <h1 class="mt-2">Add Color</h1>
           <hr>
         </div>
         <div class="card-body">
-          <form action="{{ route('admin.auth') }}" method="post">
-            @csrf
-            <div class="form-group mb-3">
-              <label for="name" class="form-label">Color</label>
-              <input type="name" class="form-control @error('name')
-                                                  is-invalid
-                                                @enderror" id="name" name="name" placeholder="Enter Name">
-              @error('name')
-              <span class="invalid-feedback">
-                <strong>{{ $message }}</strong>
-              </span>
-              @enderror
+          <div class="row">
+            <div class="col-md-6 mx-auto">
+              <form action="{{ route('admin.colors.store') }}" method="post">
+                @csrf
+                <div class="form-group mb-3">
+                  <label for="name" class="form-label">Color</label>
+                  <input type="name" class="form-control @error('name')
+                                                      is-invalid
+                                                    @enderror" id="name" name="name" placeholder="Enter Name">
+                  @error('name')
+                  <span class="invalid-feedback">
+                    <strong>{{ $message }}</strong>
+                  </span>
+                  @enderror
+                </div>
+                <button type="submit" class="btn btn-dark d-block mx-auto">Submit</button>
+              </form>
             </div>
-            <button type="submit" class="btn btn-dark d-block mx-auto">Submit</button>
-          </form>
+          </div>
         </div>
       </div>
     </div>
