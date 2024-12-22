dec3e0cd440ba3be9e937ca9cee43b0532ef257e
imran <zunaeede@gmail.com>
2024-12-22 10:26:56 +0600

    main - set delete button - but have got an error.

--- resources/views/admin/colors/index.blade.php
+++ resources/views/admin/colors/index.blade.php
@@ -10,19 +10,21 @@ Add new color
   <div class="col-md-9">
     <div class="row mt-2">
       <div class="col-md-12">
-        <div class="card-header bg-white">
+        <div class="card-header bg-white d-flex justify-content-between align-items-center">
           <h1 class="mt-2">Colors ({{$colors->count()}})</h1>
-          <hr>
+          <a href="{{route('admin.colors.create')}}" class="btn btn-sm btn-primary">
+            <i class="fas fa-plus"></i>
+          </a>
         </div>
+        <hr>
         {{-- table of colors --}}
         <div class="card-body">
           <table class="table">
             <thead>
               <tr>
                 <th scope="col">#</th>
-                <th scope="col">First</th>
-                <th scope="col">Last</th>
-                <th scope="col">Handle</th>
+                <th scope="col">Name</th>
+                <th scope="col">Option</th>
               </tr>
             </thead>
             <tbody>
@@ -30,6 +32,18 @@ Add new color
               <tr>
                 <th scope="row">{{$color->id}}</th>
                 <td>{{$color->name}}</td>
+                <td>
+                  <a href="{{route('admin.colors.edit', $color->id)}}" class="btn btn-sm btn-warning">
+                    <i class="fas fa-edit"></i>
+                  </a>
+                  <a href="#" onclick="deleteItem({{$color->id}})" class="btn btn-sm btn-danger">
+                    <i class="fas fa-trash"></i>
+                  </a>
+                  <form id="{{$color->id}}" action="{{('admin.colors.destroy', $color->id)}}" method="post">
+                    @csrf
+                    @method('DELETE')
+                  </form>
+                </td>
               </tr>
               @endforeach
             </tbody>
