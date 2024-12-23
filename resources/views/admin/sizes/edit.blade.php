41668e9dcc847d85e9db31002d301589f1d4fe17
imran <zunaeede@gmail.com>
2024-12-23 10:32:29 +0600

    main - make the edit sixe page.

--- resources/views/admin/sizes/edit.blade.php
+++ resources/views/admin/sizes/edit.blade.php
@@ -1,7 +1,7 @@
 @extends('admin.layouts.app')
 
 @section('title')
-Edit Color
+Edit Size
 @endsection
 
 @section('content')
@@ -11,25 +11,25 @@ Edit Color
     <div class="row mt-2">
       <div class="col-md-12">
         <div class="card-header bg-white">
-          <h1 class="mt-2">Edit Color</h1>
+          <h1 class="mt-2">Edit Size</h1>
           <hr>
         </div>
         <div class="card-body">
           <div class="row">
             <div class="col-md-6 mx-auto">
-              <form action="{{ route('admin.colors.update', $color->id) }}" method="post">
+              <form action="{{ route('admin.sizes.update', $size->id) }}" method="post">
                 @csrf
                 @method('PUT')
                 <div class="form-group mb-3">
-                  <label for="name" class="form-label">Edit Name</label>
+                  <label for="name" class="form-label">Edit Size</label>
                   <input type="text" class="form-control 
                     @error('name')
                       is-invalid
                     @enderror" 
                       id="name" 
                       name="name" 
-                      placeholder="Enter Name"
-                      value="{{$color->name, old('name')}}">
+                      placeholder="Enter Size"
+                      value="{{$size->name, old('name')}}">
                     @error('name')
                       <span class="invalid-feedback">
                         <strong>{{ $message }}</strong>
