528e5564f23a36ba00a8fc01afb6ef68173a2158
imran <zunaeede@gmail.com>
2024-12-23 10:18:10 +0600

    main - set create page for size.

--- resources/views/admin/sizes/create.blade.php
+++ resources/views/admin/sizes/create.blade.php
@@ -1,7 +1,7 @@
 @extends('admin.layouts.app')
 
 @section('title')
-Add new color
+Add new size
 @endsection
 
 @section('content')
@@ -11,19 +11,19 @@ Add new color
     <div class="row mt-2">
       <div class="col-md-12">
         <div class="card-header bg-white">
-          <h1 class="mt-2">Add Color</h1>
+          <h1 class="mt-2">Add Size</h1>
           <hr>
         </div>
         <div class="card-body">
           <div class="row">
             <div class="col-md-6 mx-auto">
-              <form action="{{ route('admin.colors.store') }}" method="post">
+              <form action="{{ route('admin.sizes.store') }}" method="post">
                 @csrf
                 <div class="form-group mb-3">
-                  <label for="name" class="form-label">Color</label>
+                  <label for="name" class="form-label">Size</label>
                   <input type="name" class="form-control @error('name')
                                                       is-invalid
-                                                    @enderror" id="name" name="name" placeholder="Enter Name" value="{{old('name')}}">
+                                                    @enderror" id="name" name="name" placeholder="Enter Size" value="{{old('name')}}">
                   @error('name')
                   <span class="invalid-feedback">
                     <strong>{{ $message }}</strong>
