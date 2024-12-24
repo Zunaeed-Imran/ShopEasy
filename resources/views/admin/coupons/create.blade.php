edf00e225c384f389c17952c0f7838e56709c287
imran <zunaeede@gmail.com>
2024-12-24 11:38:41 +0600

    main - make add new coupon page.

--- resources/views/admin/coupons/create.blade.php
+++ resources/views/admin/coupons/create.blade.php
@@ -1,7 +1,7 @@
 @extends('admin.layouts.app')
 
 @section('title')
-Add new size
+Add new coupon
 @endsection
 
 @section('content')
@@ -11,20 +11,44 @@ Add new size
     <div class="row mt-2">
       <div class="col-md-12">
         <div class="card-header bg-white">
-          <h1 class="mt-2">Add Size</h1>
+          <h1 class="mt-2">Add Coupon</h1>
           <hr>
         </div>
         <div class="card-body">
           <div class="row">
             <div class="col-md-6 mx-auto">
-              <form action="{{ route('admin.sizes.store') }}" method="post">
+              <form action="{{ route('admin.coupons.store') }}" method="post">
                 @csrf
                 <div class="form-group mb-3">
-                  <label for="name" class="form-label">Size</label>
+                  <label for="name" class="form-label">Coupon</label>
                   <input type="name" class="form-control @error('name')
                                                       is-invalid
-                                                    @enderror" id="name" name="name" placeholder="Enter Size" value="{{old('name')}}">
+                                                    @enderror" id="name" name="name" placeholder="Enter Coupon" value="{{old('name')}}">
                   @error('name')
+                  <span class="invalid-feedback">
+                    <strong>{{ $message }}</strong>
+                  </span>
+                  @enderror
+                </div>
+                {{-- Discount --}}
+                <div class="form-group mb-3">
+                  <label for="name" class="form-label">Discount</label>
+                  <input type="name" class="form-control @error('discount')
+                                                      is-invalid
+                                                    @enderror" id="discount" name="discount" placeholder="Discount" value="{{old('discount')}}">
+                  @error('discount')
+                  <span class="invalid-feedback">
+                    <strong>{{ $message }}</strong>
+                  </span>
+                  @enderror
+                </div>
+                {{-- Validation --}}
+                <div class="form-group mb-3">
+                  <label for="name" class="form-label">Validity</label>
+                  <input type="datetime-local" class="form-control @error('valid_until')
+                                                      is-invalid
+                                                    @enderror" id="valid_until" name="valid_until" placeholder="Validity" value="{{old('valid_until')}}">
+                  @error('valid_until')
                   <span class="invalid-feedback">
                     <strong>{{ $message }}</strong>
                   </span>
