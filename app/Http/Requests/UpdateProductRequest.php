0429c6f9d808bc3d657e3906c7331633d24c53e9
imran <zunaeede@gmail.com>
2024-12-28 10:17:49 +0600

    main - set reule and message in update products.

--- app/Http/Requests/UpdateProductRequest.php
+++ app/Http/Requests/UpdateProductRequest.php
@@ -11,7 +11,7 @@ class UpdateProductRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -23,6 +23,35 @@ class UpdateProductRequest extends FormRequest
     {
         return [
             //
+            'name' => 'required|max:255|unique:products,name,'.$this->product->id,
+            'qty' => 'required|numeric',
+            'price' => 'required|numeric',
+            'color_id' => 'required|numeric',
+            'size_id' => 'required|numeric',
+            'desc' => 'required|max:2000',
+            'thumbnail' => 'image|mimes:png,jpg,jpeg|max:2048',
+            'first_image' => 'image|mimes:png,jpg,jpeg|max:2048',
+            'second_image' => 'image|mimes:png,jpg,jpeg|max:2048',
+            'third_image' => 'image|mimes:png,jpg,jpeg|max:2048',
+        ];
+    }
+    public function messages()
+    {
+        return [
+            //
+            'color_id.required' => 'The color filed is required',
+            'size_id.required' => 'The size filed is required',
+            'desc.required' => 'The description filed is required',
+            'desc.max' => 'The description must not be greater than :max characters',
+            'qty.required' => 'The quantity filed is required',
+            'thumbnail.max' => 'The thumbnail must not be greater than 2MB',
+            'thumbnail.image' => 'The thumbnail must be an image',
+            'first_image.image' => 'The first_image must be an image',
+            'second_image.image' => 'The second_image must be an image',
+            'third_image.image' => 'The third_image must be an image',
+            'first_image.max' => 'The first_image must not be greater than 2MB',
+            'second_image.max' => 'The second_image must not be greater than 2MB',
+            'third_image.max' => 'The third_image must not be greater than 2MB',
         ];
     }
 }
