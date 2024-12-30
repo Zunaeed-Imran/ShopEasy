71a231e1283d6f52c7231e872d43dadc972a22e4
imran <zunaeede@gmail.com>
2024-12-30 13:45:38 +0600

    main - make the first api route.

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Http\Controllers\Api\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+Route::get('products', [ProductController::class, 'index']);
+Route::get('products/{color}/color', [ProductController::class, 'filterProductsByColor']);
+Route::get('products/{size}/size', [ProductController::class, 'filterProductsBySize']);
+Route::get('products/{searchTerm}/find', [ProductController::class, 'findProductsByTerm']);
+Route::get('products/{product}/show', [ProductController::class, 'show']);
