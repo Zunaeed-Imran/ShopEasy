4f4abff84777900bc40c97068c9598c2f2bdbbd2
imran <zunaeede@gmail.com>
2024-12-30 18:24:28 +0600

    main - working on home page.

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,8 +1,26 @@
+import { useEffect, useState } from "react"
 
 export default function Home() {
+  const [products, setProducts] = useState([])
+  const [colors, setColors] = useState([])
+  const [sizes, setSizes] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    const fetchAllProducts = async () => {
+      try {
+        const response = await axios.get('')
+        console.log(response.data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchAllProducts()
+  }, [])
+
   return (
     <div>
-      
+      Home
     </div>
   )
 }
