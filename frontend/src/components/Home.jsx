38ca285d0383d62b92a603d4470e51a73a48ced5
imran <zunaeede@gmail.com>
2025-01-05 18:55:18 +0600

    main - set teh filtering option.

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -12,27 +12,132 @@ export default function Home() {
   const [colors, setColors] = useState([])
   const [sizes, setSizes] = useState([])
   const [loading, setLoading] = useState(false)
+  const [selectedColor, setSelectedColor] = useState('')
+  const [selectedSize, setSelectedSize] = useState('')
+  const [searchTerm, setSearchTerm] = useState('')
+  const [message, setMessage] = useState('')
+
+  const handleColorSelectBox = (e) => {
+    setSelectedSize('')
+    setSearchTerm('')
+    setSelectedColor(e.target.value)
+  }
+  const handleSizeSelectBox = (e) => {
+    setSelectedColor('')
+    setSearchTerm('')
+    setSelectedSize(e.target.value)
+  }
+  const clearFilters = () => {
+    setSelectedColor('')
+    setSelectedSize('')
+  }
 
   useEffect(() => {
     const fetchAllProducts = async () => {
       try {
-        const response = await axiosRequest.get(
-          'products'
-        );
-        setProducts(response.data.data)
-        setColors(response.data.colors)
-        setSizes(response.data.sizes);
-        console.log(response.data.data)
+        if (selectedColor) {
+          const response = await axiosRequest.get(`products/${selectedColor}/color`);
+          setProducts(response.data.data);
+          setColors(response.data.colors);
+          setSizes(response.data.sizes);
+        } else if (selectedSize) {
+          const response = await axiosRequest.get(
+            `products/${selectedSize}/size`
+          );
+          setProducts(response.data.data);
+          setColors(response.data.colors);
+          setSizes(response.data.sizes);
+        } else {
+          const response = await axiosRequest.get('products');
+          setProducts(response.data.data);
+          setColors(response.data.colors);
+          setSizes(response.data.sizes);
+        }
       } catch (error) {
         console.log(error)
       }
     }
     fetchAllProducts()
-  }, [])
+  }, [selectedColor, selectedSize, searchTerm])
 
   return (
-    <div className="">
-      <ProductsList products={products} />
+    <div className="row my-5">
+      <div className="col-md-12">
+        <div className="row">
+          <div className="col-md-8 mx-auto">
+            <div className="row">
+              <div className="col-md-4 md-2">
+                <div className="mb-2">
+                  <span className="fw-bold">Filter by color:</span>
+                </div>
+                <select
+                  name="color_id"
+                  id="color_id"
+                  defaultValue={''}
+                  onChange={e => handleColorSelectBox(e)}
+                  disabled={selectedColor || searchTerm}
+                  className="form-select"
+                >
+                  <option
+                    value=""
+                    disabled={!selectedColor}
+                    onChange={() => clearFilters()}
+                  >
+                    All Colors
+                  </option>
+                  {colors.map(color => (
+                    <option value={color.id} key={color.id}>
+                      {color.name}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <div className="col-md-4 md-2">
+                <div className="mb-2">
+                  <span className="fw-bold">Filter by size:</span>
+                </div>
+                <select
+                  name="size_id"
+                  id="size_id"
+                  defaultValue={''}
+                  onChange={e => handleSizeSelectBox(e)}
+                  disabled={selectedSize || searchTerm}
+                  className="form-select"
+                >
+                  <option
+                    value=""
+                    disabled={!selectedSize}
+                    onChange={() => clearFilters()}
+                  >
+                    All Sizes
+                  </option>
+                  {sizes.map(size => (
+                    <option value={size.id} key={size.id}>
+                      {size.name}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <div className="col-md-4 md-2">
+                <div className="mb-2">
+                  <span className="fw-bold">Search:</span>
+                </div>
+                <form className="d-flex">
+                  <input
+                    type="search"
+                    className="form-control me-2"
+                    value={searchTerm}
+                    // disabled={selectedColor || selectedSize}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                    placeholder="Search"
+                  />
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+        <ProductsList products={products} />
+      </div>
     </div>
-  )
+  );
 }
