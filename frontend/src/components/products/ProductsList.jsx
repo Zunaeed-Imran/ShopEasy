dc5e77cb27c23c3190806265d5cd2885d889708e
imran <zunaeede@gmail.com>
2025-01-07 12:17:20 +0600

    main - 5 items display only in display set condation.

--- frontend/src/components/products/ProductsList.jsx
+++ frontend/src/components/products/ProductsList.jsx
@@ -1,10 +1,23 @@
+import { useState } from "react";
 import ProductListItem from "./ProductListItem"
 
 export default function ProductsList({ products }) {
   // console.log(products);
+
+  const [productsToShow, setProductsToShow] = useState(5)
+
+  const loadMoreProducts = () => {
+    if (productsToShow > products.length) {
+      return;
+    } else {
+      setProductsToShow(preProductsToShow => preProductsToShow += 5)
+    }
+  }
+
+
   return (
     <div className="row my-5">
-      {products?.map(product => (
+      {products?.slice(0, productsToShow).map(product => (
         <ProductListItem product={product} key={product.id} />
       ))}
     </div>
