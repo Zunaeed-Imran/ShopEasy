9a84235e74f42631b346a09d4b961fbac16301bb
imran <zunaeede@gmail.com>
2025-01-14 20:57:54 +0600

    main - make some new file.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,8 @@ import Header from './components/layouts/Header'
 import Product from './components/products/Product'
 import Cart from './cart/Cart'
 import Checkout from './components/checkout/Checkout'
+import Login from './components/user/Login'
+import Register from './components/user/Register'
 
 function App() {
 
@@ -17,6 +19,8 @@ function App() {
         <Route path="/cart" element={<Cart/>} />
         <Route path="/product/:slug" element={<Product />} />
         <Route path='/checkout' element={<Checkout/>} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/register' element={<Register/>} />
       </Routes>
     </BrowserRouter>
   )
