0d96807a3ece08af5c44341b6a1b535516d07062
imran <zunaeede@gmail.com>
2025-01-15 16:40:43 +0600

    main - set the unique for the registration users.

--- backend/app/Http/Requests/StoreUserRequest.php
+++ backend/app/Http/Requests/StoreUserRequest.php
@@ -24,7 +24,7 @@ class StoreUserRequest extends FormRequest
         return [
             //
             'name' => 'required|max:255',
-            'email' => 'required|email|max:255',
+            'email' => 'required|email|max:255|unique:users',
             'password' => 'required|min:6|max:255'
         ];
     }
