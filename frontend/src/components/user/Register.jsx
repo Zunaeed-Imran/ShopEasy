e28c6fd5ee14a7d064e15a666a28def833bfc522
imran <zunaeede@gmail.com>
2025-01-15 10:27:55 +0600

    main - make the frontend registration page done.

--- frontend/src/components/user/Register.jsx
+++ frontend/src/components/user/Register.jsx
@@ -23,17 +23,17 @@ export default function Register() {
           <div className="card-body">
             <form action="" method="post" className="mt-5">  
               <div className="mb-3">
-                <label  className="form-label">Email address</label>
+                <label  className="form-label">Name*</label>
+                <input type="text" className="form-control" id="name" />
+              </div>
+              <div className="mb-3">
+                <label  className="form-label">Email address*</label>
                 <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
                 <div id="emailHelp" className="form-text">Well never share your email with anyone else.</div>
               </div>
               <div className="mb-3">
-                <label className="form-label">Password</label>
+                <label className="form-label">Password*</label>
                 <input type="password" className="form-control" id="exampleInputPassword1" />
-              </div>
-              <div className="mb-3 form-check">
-                <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                <label className="form-check-label">Check me out</label>
               </div>
               <button type="submit" className="btn btn-primary">Submit</button>
             </form>
