7b0446ad9cb0ad726aeafefed720448bf11daeae
imran <zunaeede@gmail.com>
2025-01-15 16:35:34 +0600

    main - added the validation to show in react frontend.

--- frontend/src/components/custom/useValidations.jsx
+++ frontend/src/components/custom/useValidations.jsx
@@ -2,13 +2,13 @@
 export default function useValidations(errors, filed) {
 
 
-    const renderErrors = (filed) => {
+    const renderErrors = (filed) => (
       errors?.[filed]?.map((error, index) => (
         <div key={index} className="text-white my-2 rounded p-2 bg-danger">
           {error}
         </div>
       ))
-    }
+    )
 
   return renderErrors(filed)
 }
--- frontend/src/components/user/Register.jsx
+++ frontend/src/components/user/Register.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom"
 import { axiosRequest } from "../../helper/config"
 import { toast } from "react-toastify"
 import Spinner from "../layouts/Spinner"
+import useValidations from "../custom/useValidations"
 
 export default function Register() {
 
@@ -11,8 +12,10 @@ export default function Register() {
     email: '',
     password: ''
   })
+  const [validationErrors, setValidationErrors] = useState([])
   const [loading, setLoading] = useState(false)
   const navigate = useNavigate()
+
   const registerNewUser = async (e) => {
     e.preventDefault()
     setLoading(true);
@@ -22,6 +25,9 @@ export default function Register() {
       toast.success(response.data.message)
       navigate('/login')
     } catch (error) {
+      if (error?.response?.status === 422) {
+        setValidationErrors(error.response.data.errors)
+      }
       console.log(error)
       setLoading(false)
     }
@@ -32,41 +38,50 @@ export default function Register() {
       <div className="col-md-6 mx-auto">
         <div className="card shadow-sm">
           <div className="card-header bg-white">
-            <h5 className="text-center mt-2">
-              Register
-            </h5>
+            <h5 className="text-center mt-2">Register</h5>
           </div>
           <div className="card-body">
             <form
               action=""
               method="post"
               className="mt-5"
-              onSubmit={(e) => registerNewUser(e)}
-            >  
+              onSubmit={e => registerNewUser(e)}
+            >
               <div className="mb-3">
-                <label  className="form-label">Name*</label>
+                <label className="form-label">Name*</label>
                 <input
                   type="text"
-                  className="form-control"
                   id="name"
                   value={user.name}
-                  onChange={(e) => setUser({
-                    ...user, name: e.target.value
-                  })} />
+                  onChange={e =>
+                    setUser({
+                      ...user,
+                      name: e.target.value,
+                    })
+                  }
+                  className="form-control"
+                  />
+                  {useValidations(validationErrors, 'name')}
               </div>
               <div className="mb-3">
-                <label  className="form-label">Email address*</label>
+                <label className="form-label">Email address*</label>
                 <input
                   type="email"
                   className="form-control"
                   id="exampleInputEmail1"
                   aria-describedby="emailHelp"
                   value={user.email}
-                  onChange={(e) => setUser({
-                    ...user, email: e.target.value
-                  })}
+                  onChange={e =>
+                    setUser({
+                      ...user,
+                      email: e.target.value,
+                    })
+                  }
                 />
-                <div id="emailHelp" className="form-text">Well never share your email with anyone else.</div>
+                <div id="emailHelp" className="form-text">
+                  Well never share your email with anyone else.
+                </div>
+                {useValidations(validationErrors, 'email')}
               </div>
               <div className="mb-3">
                 <label className="form-label">Password*</label>
@@ -75,21 +90,26 @@ export default function Register() {
                   className="form-control"
                   id="exampleInputPassword1"
                   value={user.password}
-                  onChange={(e) => setUser({
-                    ...user, password: e.target.value
-                  })}
-                />
+                  onChange={e =>
+                    setUser({
+                      ...user,
+                      password: e.target.value,
+                    })
+                  }
+                  />
+                  {useValidations(validationErrors, 'password')}
               </div>
-              {
-                loading ?
-                  <Spinner />
-                  :
-              <button type="submit" className="btn btn-dark btn-sm">Submit</button>
-              }
+              {loading ? (
+                <Spinner />
+              ) : (
+                <button type="submit" className="btn btn-dark btn-sm">
+                  Submit
+                </button>
+              )}
             </form>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
