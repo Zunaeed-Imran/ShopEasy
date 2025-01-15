b9d66f30738ab06420ab3b91e3ac20eefeaefa54
imran <zunaeede@gmail.com>
2025-01-15 17:13:20 +0600

    main - set the validation for login page.

--- frontend/src/components/user/Login.jsx
+++ frontend/src/components/user/Login.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import { axiosRequest } from "../../helper/config";
 import { toast } from "react-toastify";
 import Spinner from "../layouts/Spinner";
+import useValidations from "../custom/useValidations";
 
 export default function Login() {
 
@@ -10,10 +11,12 @@ export default function Login() {
       email: '',
       password: ''
     })
+    const [validationErrors, setValidationErrors] = useState([]);
     const [loading, setLoading] = useState(false);
     const navigate = useNavigate();
     const loginUser = async e => {
       e.preventDefault();
+      setValidationErrors([])
       setLoading(true);
       try {
         const response = await axiosRequest.post('user/login', user);
@@ -25,62 +28,72 @@ export default function Login() {
           navigate('/');
         }
       } catch (error) {
+        if (error?.response?.status === 422) {
+          setValidationErrors(error.response.data.errors);
+        }
         console.log(error);
         setLoading(false);
       }
     };
 
   return (
-        <div className="row my-5">
-          <div className="col-md-6 mx-auto">
-            <div className="card shadow-sm">
-              <div className="card-header bg-white">
-                <h5 className="text-center mt-2">
-                  Login
-                </h5>
-              </div>
-              <div className="card-body">
-                <form
-                  action=""
-                  method="post"
-                  className="mt-5"
-                  onSubmit={(e) => loginUser(e)}
-                >
-                  <div className="mb-3">
-                    <label  className="form-label">Email address*</label>
-                    <input
-                      type="email"
-                      className="form-control"
-                      id="exampleInputEmail1"
-                      aria-describedby="emailHelp"
-                      value={user.email}
-                      onChange={(e) => setUser({
-                        ...user, email: e.target.value
-                      })}
-                    />
-                  </div>
-                  <div className="mb-3">
-                    <label className="form-label">Password*</label>
-                    <input
-                      type="password"
-                      className="form-control"
-                      id="exampleInputPassword1"
-                      value={user.password}
-                      onChange={(e) => setUser({
-                        ...user, password: e.target.value
-                      })}
-                    />
-                  </div>
-                  {
-                    loading ?
-                      <Spinner />
-                      :
-                  <button type="submit" className="btn btn-dark btn-sm">Submit</button>
+    <div className="row my-5">
+      <div className="col-md-6 mx-auto">
+        <div className="card shadow-sm">
+          <div className="card-header bg-white">
+            <h5 className="text-center mt-2">Login</h5>
+          </div>
+          <div className="card-body">
+            <form
+              action=""
+              method="post"
+              className="mt-5"
+              onSubmit={e => loginUser(e)}
+            >
+              <div className="mb-3">
+                <label className="form-label">Email address*</label>
+                <input
+                  type="email"
+                  className="form-control"
+                  id="exampleInputEmail1"
+                  aria-describedby="emailHelp"
+                  value={user.email}
+                  onChange={e =>
+                    setUser({
+                      ...user,
+                      email: e.target.value,
+                    })
                   }
-                </form>
+                />
+                {useValidations(validationErrors, 'email')}
               </div>
-            </div>
+              <div className="mb-3">
+                <label className="form-label">Password*</label>
+                <input
+                  type="password"
+                  className="form-control"
+                  id="exampleInputPassword1"
+                  value={user.password}
+                  onChange={e =>
+                    setUser({
+                      ...user,
+                      password: e.target.value,
+                    })
+                  }
+                />
+                {useValidations(validationErrors, 'password')}
+              </div>
+              {loading ? (
+                <Spinner />
+              ) : (
+                <button type="submit" className="btn btn-dark btn-sm">
+                  Submit
+                </button>
+              )}
+            </form>
           </div>
         </div>
-  )
+      </div>
+    </div>
+  );
 }
