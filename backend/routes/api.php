e58b8000e535f16720e8627b304a614ef41813fe
imran <zunaeede@gmail.com>
2025-01-20 13:15:34 +0600

    main - api user path fix.

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function(){
 
-  Route::get('/user', function (Request $request) {
+  Route::get('user', function (Request $request) {
       return [
         'user' => UserResource::make($request->user()),
         'access_token' => $request->bearerToken()
