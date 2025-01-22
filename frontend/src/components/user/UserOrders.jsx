177cfe5778512adee475f5084389fa8573c0b8e1
imran <zunaeede@gmail.com>
2025-01-22 20:37:01 +0600

    main - set the warning message in the table to look good.

--- frontend/src/components/user/UserOrders.jsx
+++ frontend/src/components/user/UserOrders.jsx
@@ -27,67 +27,72 @@ export default function UserOrders() {
       <ProfileSidebar />
       <div className="col-md-8">
         <div className="card-body">
-          <table className="table">
-            <thead>
-              <tr>
-                <th>#</th>
-                <th>Product Name</th>
-                <th>Product Price</th>
-                <th>Qty</th>
-                <th>Total</th>
-                <th>Orderd Date</th>
-                <th>Delivered Date</th>
-              </tr>
-            </thead>
-            <tbody>
-              {
-                user?.orders.length > 0
-                  ?
-                                  user?.orders?.slice(0, ordersToShow).map((order, index) => (
-                <tr>
-                  <th>{index + 1}</th>
-                  <th>
-                    <div className="d-flex flex-column">
-                      {order?.products?.map(product => (
-                        <span className="badge bg-success my-1 rounded-0">
-                          {product.name}
-                        </span>
-                      ))}
-                    </div>
-                  </th>
-                  <th>
-                    <div className="d-flex flex-column">
-                      {order?.products?.map(product => (
-                        <span className="badge bg-danger my-1 rounded-0">
-                          {product.price}
-                        </span>
-                      ))}
-                    </div>
-                  </th>
-                  <th>{order.qty}</th>
-                  <th>
-                    <span className="badge bg-secondary my-1 rounded-0">
-                      ${order.total}
-                    </span>
-                  </th>
-                  <th>{order.created_at}</th>
-                  <th>
-                    {order.delivered_at ? (
-                      <span className="badge bg-success my-1 rounded-0">
-                        {product.delivered_at}
-                      </span>
-                    ) : (
-                      <i className="text-muted">Pending...</i>
-                    )}
-                  </th>
-                </tr>
-                ))
-                  :
-                  <Alert content="No Orders Yet" type="primary" />
-              }
-            </tbody>
-            </table>
-            {ordersToShow < user?.order?.length && (
+          {
+            user?.orders?.length > 0
+          
+              ?
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th>#</th>
+                    <th>Product Name</th>
+                    <th>Product Price</th>
+                    <th>Qty</th>
+                    <th>Total</th>
+                    <th>Orderd Date</th>
+                    <th>Delivered Date</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {
+                
+                    user?.orders?.slice(0, ordersToShow).map((order, index) => (
+                      <tr>
+                        <th>{index + 1}</th>
+                        <th>
+                          <div className="d-flex flex-column">
+                            {order?.products?.map(product => (
+                              <span className="badge bg-success my-1 rounded-0">
+                                {product.name}
+                              </span>
+                            ))}
+                          </div>
+                        </th>
+                        <th>
+                          <div className="d-flex flex-column">
+                            {order?.products?.map(product => (
+                              <span className="badge bg-danger my-1 rounded-0">
+                                {product.price}
+                              </span>
+                            ))}
+                          </div>
+                        </th>
+                        <th>{order.qty}</th>
+                        <th>
+                          <span className="badge bg-secondary my-1 rounded-0">
+                            ${order.total}
+                          </span>
+                        </th>
+                        <th>{order.created_at}</th>
+                        <th>
+                          {order.delivered_at ? (
+                            <span className="badge bg-success my-1 rounded-0">
+                              {product.delivered_at}
+                            </span>
+                          ) : (
+                            <i className="text-muted">Pending...</i>
+                          )}
+                        </th>
+                      </tr>
+                    ))
+                  }
+                </tbody>
+              </table>
+              :
+              <Alert content="No Orders Yet" type="primary" />
+          }
+          {
+            ordersToShow < user?.order?.length && (
               <div className="d-flex justify-content-center my-3">
                 <button
                   className="btn btn-sm btn-dark"
